8679df445961f5ac86134c5ff9115131310c306c
MorikawaRei <93118883+MorikawaRei@users.noreply.github.com>
2023-02-08 22:39:22 +0900

    tutorial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<!-- scriptで変数を作る -->
+<script>
+	// name変数を宣言する
+	let name = 'world';
+	// src変数
+	let src = 'favicon.png';
+	// string変数
+	let string = `this string contains some <strong>HTML!!!</strong>`;
+</script>
 
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<!-- Hello world!にtailwindcssでスタイルをつける -->
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<!-- マークアップからnameを参照する -->
+<h2>Hello {name}!</h2>
+<!-- srcを参照 -->
+<img {src} alt="svelte" />
+<!-- {@html ...} という特別なタグを使う https://svelte.jp/tutorial/html-tags -->
+<p>{@html string}</p>
