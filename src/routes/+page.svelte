91857b37f2e7c019cf0b5f08f449df853059fdd3
MorikawaRei <93118883+MorikawaRei@users.noreply.github.com>
2023-02-10 20:08:37 +0900

    count-comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,14 @@
 	let src = 'favicon.png';
 	// string変数
 	let string = `this string contains some <strong>HTML!!!</strong>`;
-
+	// count変数
 	let count = 0;
+	// count変数を二倍にするにしたものdoubled変数に入れる
+	// $:意味がわからない,letにすると計算してくれない（だから計算白的な意味？）
 	$: doubled = count * 2;
 
+	// HTML側の引数handleClickが押されたとき
+	// functionはjsの引数に指定したものに返すもの？
 	function handleClick() {
 		count += 1;
 	}
@@ -24,8 +28,12 @@
 <!-- {@html ...} という特別なタグを使う https://svelte.jp/tutorial/html-tags -->
 <p>{@html string}</p>
 
+<!--　handleClickはbuttonが押されたときの動き -->
+<!-- on:clickはボタンがclickしたときに起動する処理(handleClick変数の処理) -->
 <button on:click={handleClick}>
+	<!-- ?がif文っぽいtrue->time false->times -->
 	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
 
+<!-- count変数の中とcountの値を２倍したdoubled変数 -->
 <p>{count} doubled is {doubled}</p>
